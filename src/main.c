961aee42940d59b471e28091748af34995fd8e47
mondaugen <nicholas.esterer@gmail.com>
2014-07-28 19:08:31 -0400

    initial commit

---
+++ src/main.c
@@ -0,0 +1,141 @@
+/**
+  ******************************************************************************
+  * @file    IO_Toggle/main.c 
+  * @author  MCD Application Team
+  * @version V1.0.0
+  * @date    19-September-2011
+  * @brief   Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * THE PRESENT FIRMWARE WHICH IS FOR GUIDANCE ONLY AIMS AT PROVIDING CUSTOMERS
+  * WITH CODING INFORMATION REGARDING THEIR PRODUCTS IN ORDER FOR THEM TO SAVE
+  * TIME. AS A RESULT, STMICROELECTRONICS SHALL NOT BE HELD LIABLE FOR ANY
+  * DIRECT, INDIRECT OR CONSEQUENTIAL DAMAGES WITH RESPECT TO ANY CLAIMS ARISING
+  * FROM THE CONTENT OF SUCH FIRMWARE AND/OR THE USE MADE BY CUSTOMERS OF THE
+  * CODING INFORMATION CONTAINED HEREIN IN CONNECTION WITH THEIR PRODUCTS.
+  *
+  * <h2><center>&copy; COPYRIGHT 2011 STMicroelectronics</center></h2>
+  ******************************************************************************  
+  */ 
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4_discovery.h"
+#include "stm32f4xx_conf.h" // again, added because ST didn't put it here ?
+
+#define MIDI_BAUD_RATE 31250
+
+GPIO_InitTypeDef  GPIO_USART_InitStruct;
+USART_InitTypeDef  USART_InitStruct;
+
+void Delay(__IO uint32_t nCount);
+void USART2_Enable_Tx(void);
+
+int main(void)
+{
+  /*!< At this stage the microcontroller clock setting is already configured, 
+       this is done through SystemInit() function which is called from startup
+       file (startup_stm32f4xx.s) before to branch to application main.
+       To reconfigure the default setting of SystemInit() function, refer to
+        system_stm32f4xx.c file
+     */
+    
+    /* Enable USART For Tx */
+    USART2_Enable_Tx();
+
+  while (1)
+  {
+      /* Send note on (middle c [key number 60], velocity of 99), channel 1 */
+      USART_SendData(USART2, 0x90);
+      USART_SendData(USART2, 60);
+      USART_SendData(USART2, 99);
+    
+      /* Insert delay */
+      Delay(0xFFFFFF);
+
+      /* Send note off */
+      USART_SendData(USART2, 0x80);
+      USART_SendData(USART2, 60);
+      USART_SendData(USART2, 0);
+
+      /* Insert delay */
+      Delay(0xFFFFFF);
+  }
+}
+
+/**
+  * @brief  Delay Function.
+  * @param  nCount:specifies the Delay time length.
+  * @retval None
+  */
+void Delay(__IO uint32_t nCount)
+{
+  while(nCount--)
+  {
+  }
+}
+
+void USART2_Enable_Tx(void)
+{
+    /* Enable Clocks */
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2, ENABLE);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+
+    /* Set up GPIO for alternate function */
+    GPIO_PinAFConfig(GPIOA,GPIO_PinSource2,GPIO_AF_USART2);
+
+    /* Configure GPIO to transmit */
+    GPIO_USART_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_USART_InitStruct.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_USART_InitStruct.GPIO_OType = GPIO_OType_OD;
+    GPIO_USART_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_USART_InitStruct.GPIO_Pin = GPIO_Pin_2;
+    GPIO_Init(GPIOA, &GPIO_USART_InitStruct);
+
+    /* Configure USART */
+    USART_InitStruct.USART_BaudRate = MIDI_BAUD_RATE;
+    USART_InitStruct.USART_WordLength = USART_WordLength_8b;
+    USART_InitStruct.USART_StopBits = USART_StopBits_1;
+    USART_InitStruct.USART_Parity = USART_Parity_No;
+    USART_InitStruct.USART_Mode = USART_Mode_Tx;
+    USART_InitStruct.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_Init(USART2, &USART_InitStruct);
+
+    /* Enable UART */
+    USART_Cmd(USART2, ENABLE);
+}
+
+
+
+
+
+#ifdef  USE_FULL_ASSERT
+
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{ 
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {
+  }
+}
+#endif
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+  */ 
+
+/******************* (C) COPYRIGHT 2011 STMicroelectronics *****END OF FILE****/
