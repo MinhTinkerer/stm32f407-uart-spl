040e4d576d4e0b772269bcbebae42adc12cd9633
Nichoas Esterer <nicholas.esterer@gmail.com>
2014-07-29 12:45:37 -0400

    figured out that HSE_VALUE not getting set properly (defined in 2 places). Fix that makefile

--- src/main.c
+++ src/main.c
@@ -46,20 +46,7 @@ int main(void)
   while (1)
   {
       /* Send note on (middle c [key number 60], velocity of 99), channel 1 */
-      USART_SendData(USART2, 0x90);
-      USART_SendData(USART2, 60);
-      USART_SendData(USART2, 99);
-    
-      /* Insert delay */
-      Delay(0xFFFFFF);
-
-      /* Send note off */
-      USART_SendData(USART2, 0x80);
-      USART_SendData(USART2, 60);
-      USART_SendData(USART2, 0);
-
-      /* Insert delay */
-      Delay(0xFFFFFF);
+      USART_SendData(USART2, 0xa5);
   }
 }
 
